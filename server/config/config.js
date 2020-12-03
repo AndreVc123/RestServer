5707e112abe0446aa4c7a71c94441c91b9c3f584
AndreVc123 <59818468+AndreVc123@users.noreply.github.com>
2020-12-03 00:34:28 -0600

    Variable de entorno momgo

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlBD;
 if(process.env.NODE_ENV === 'dev'){
     urlBD = 'mongodb://localhost:27017/cafe'; 
 }else{
-    urlBD = 'mongodb+srv://LearningCourse:Italia-12394@cluster0.upqbb.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlBD = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlBD;
